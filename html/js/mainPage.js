6733f011ed4ecd9caab47bc49268b5af1464c1c2
Proggertopper <sasakosinskij8@gmail.com>
2026-01-12 16:29:54 +0200

    Delete html directory

--- html/js/mainPage.js
+++
@@ -1,7 +0,0 @@
-
-
-if (window.location.hash && window.location.hash === "#_=_") {
-    // Убираем hash, чтобы не было в адресной строке
-    history.replaceState(null, null, window.location.href.split('#')[0]);
-}
-
