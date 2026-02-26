6cfdf6ce7187db8f4c4bb6162b4eca1a9b2a245d
Sashak <sasakosinskij8@gmail.com>
2026-02-26 18:34:26 +0000

    new3

--- html/js/metricsMoney.js
+++ html/js/metricsMoney.js
@@ -1,12 +1,3 @@
-(function (w, d, s, l, i) {
-    w[l] = w[l] || []; w[l].push({
-        'gtm.start':
-            new Date().getTime(), event: 'gtm.js'
-    }); var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-            'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-WWTZ7MNG');
-
 
 !function (f, b, e, v, n, t, s) {
     if (f.fbq) return; n = f.fbq = function () {
@@ -30,3 +21,12 @@ gtag('config', 'AW-17955150066');
 gtag('config', 'G-CPDVWW8WWR');
 gtag('config', 'GT-5R6WN9BV');
 gtag('config', 'GT-P85SLQHV');
+
+(function (w, d, s, l, i) {
+    w[l] = w[l] || []; w[l].push({
+        'gtm.start':
+            new Date().getTime(), event: 'gtm.js'
+    }); var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-WWTZ7MNG');
